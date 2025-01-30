f17e036dd19e4fc1c7dd8f1a146821e46c3a2da2
Sahit Tuntas Sadono <sahitsadono@gmail.com>
2025-01-30 07:15:51 +0700

    fix: vconsole attribute

--- app/plugins/vconsole.client.ts
+++ app/plugins/vconsole.client.ts
@@ -2,7 +2,7 @@ import VConsole from "vconsole"
 import { useAppConfig } from "~/composables/ui/app-config"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (import.meta.dev || nuxtApp.$config.public.includeVconsole === "1") {
+  if (import.meta.dev || tryParseInt(nuxtApp.$config.public?.includeVconsole) === 1) {
     // eslint-disable-next-line no-console
     console.debug("enabling VConsole")
     nuxtApp.vueApp.use(new VConsole() as Plugin<[]>)
@@ -15,3 +15,20 @@ export default defineNuxtPlugin((nuxtApp) => {
     }
   }
 })
+
+function tryParseInt(num: any) {
+  if (num == null) {
+    return 0
+  }
+
+  try {
+    const parsed = Number.parseInt(num)
+    if (Number.isNaN(parsed)) {
+      return 0
+    }
+    return parsed
+  }
+  catch {
+    return 0
+  }
+}
