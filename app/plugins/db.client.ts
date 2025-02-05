58c9b58ac12be9752213e9d632ce224613e05a1b
Sahit Tuntas Sadono <sahitsadono@gmail.com>
2025-02-05 23:04:06 +0700

    refactor: migrate on create and remove try persist

--- app/plugins/db.client.ts
+++ app/plugins/db.client.ts
@@ -1,22 +1,19 @@
 import { migrateDatabase, trackProjectDataChanges } from "~/composables/project/db"
-import { tryPersistWithoutPromptingUser } from "~/composables/project/persist-db"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook("app:mounted", async () => {
-    try {
-      const persistStatus = await tryPersistWithoutPromptingUser()
-      if (persistStatus === "prompt") {
-        await navigator.storage.persist()
-      }
+  let migrated = false
+  nuxtApp.hook("app:created", () => {
+    if (import.meta.client) {
+      migrated = true
+      migrateDatabase()
+      trackProjectDataChanges()
     }
-    catch (e) {
-      // eslint-disable-next-line no-console
-      console.debug(e)
-      // eslint-disable-next-line no-console
-      console.debug("unable to turn on persist")
-    }
+  })
 
-    migrateDatabase()
-    trackProjectDataChanges()
+  nuxtApp.hook("app:mounted", async () => {
+    if (!migrated) {
+      migrateDatabase()
+      trackProjectDataChanges()
+    }
   })
 })
