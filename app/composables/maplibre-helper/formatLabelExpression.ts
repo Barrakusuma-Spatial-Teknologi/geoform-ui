dafa36f29c4b1cc4a59aa86762de80a3eeb9a6b9
muprudentio@gmail.com <muprudentio@gmail.com>
2025-03-07 11:13:18 +0700

    fix: change parameter's name on formatLabelExpression function

--- app/composables/maplibre-helper/formatLabelExpression.ts
+++ app/composables/maplibre-helper/formatLabelExpression.ts
@@ -1,5 +1,5 @@
 import type { ExpressionSpecification } from "maplibre-gl"
 
-export function formatLabelExpression(x: string[]) {
-  return ["concat", ...x.flatMap((col) => [["get", col], "\n"] as unknown as ExpressionSpecification).slice(0, -1)] as ExpressionSpecification
+export function formatLabelExpression(labelField: string[]) {
+  return ["concat", ...labelField.flatMap((field) => [["get", field], "\n"] as unknown as ExpressionSpecification).slice(0, -1)] as ExpressionSpecification
 }
